dfb8e2754f02cfb87997437b3c6a5c07a306eb5f
DanielKremes <danielkremes06@gmail.com>
2025-07-02 21:37:06 -0300

    model scheduling :  model scheduling completed

---
+++ src/main/java/br/com/multiple/payment/services/model/Scheduling.java
@@ -0,0 +1,38 @@
+package br.com.multiple.payment.services.model;
+
+import jakarta.persistence.*;
+import org.apache.logging.log4j.CloseableThreadContext;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_scheduling")
+public class Scheduling {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "scheduling_id")
+    private UUID id;
+
+    @Column(name = "destination_scheduling", nullable = false)
+    private String destinationScheduling;
+
+    @Column(name = "message_scheduling", nullable = false, columnDefinition = "TEXT")
+    private String messageScheduling;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type_comunication_scheduling",nullable = false)
+    private TypeComunication typeComunication;
+
+    @Column(name = "date_time_sent", nullable = false)
+    private Instant dateTimeSent;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_scheduling",nullable = false)
+    private StatusScheduling statusScheduling;
+
+    @Column(name = "created_scheduling", nullable = false)
+    private Instant createAt;
+
+}
