87ee4c4613f081dee0e06a9ad19c1e2d4267f23e
DanielKremes <danielkremes06@gmail.com>
2025-07-02 22:16:04 -0300

    fix(repository): add missing @Repository annotation to UserRepository

--- src/main/java/br/com/multiple/payment/services/repository/SchedulingRepository.java
+++ src/main/java/br/com/multiple/payment/services/repository/SchedulingRepository.java
@@ -2,8 +2,10 @@ package br.com.multiple.payment.services.repository;
 
 import br.com.multiple.payment.services.model.Scheduling;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface SchedulingRepository extends JpaRepository<Scheduling, UUID> {
 }
---
+++ src/main/java/br/com/multiple/payment/services/service/SchedulingService.java
@@ -0,0 +1,8 @@
+package br.com.multiple.payment.services.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SchedulingService {
+
+}
