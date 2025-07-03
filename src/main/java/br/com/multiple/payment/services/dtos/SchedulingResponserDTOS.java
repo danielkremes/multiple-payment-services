04792c0082ebbfbcf28f5fd8ff391259f6623969
DanielKremes <danielkremes06@gmail.com>
2025-07-02 22:11:19 -0300

    feat(dto)Add SchedulingResponseDTOS for handling response client

--- src/main/java/br/com/multiple/payment/services/dtos/SchedulingResponserDTOS.java
+++ src/main/java/br/com/multiple/payment/services/dtos/SchedulingResponserDTOS.java
@@ -1,4 +1,18 @@
 package br.com.multiple.payment.services.dtos;
 
-public record SchedulingResponserDTOS() {
+import br.com.multiple.payment.services.enums.StatusScheduling;
+import br.com.multiple.payment.services.enums.TypeCommunication;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record SchedulingResponserDTOS(
+        UUID id,
+        String destinationScheduling,
+        String messageScheduling,
+        TypeCommunication typeCommunication,
+        Instant dateTimeSent,
+        StatusScheduling statusScheduling,
+        Instant createAt
+) {
 }
